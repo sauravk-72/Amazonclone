185222a7572c3cc48f1bae327225009acb2ffe65
Saurav <sauravk72@gmail.com>
2023-11-03 12:29:41 +0530

    server/client added

---
+++ client/src/components/redux/actions/Action.js
@@ -0,0 +1,15 @@
+export const getProducts =()=>async(dispatch)=>{
+    try {
+        const data =await fetch("http://amazonclonefrontbysk.onrender.com/getproducts",{
+            method :"GET",
+            headers:{
+                "Content-type":"application/json"
+            }
+        });
+        const res=await data.json();
+        console.log(res)
+        dispatch({type:"SUCCESS_GET_PRODUCTS",payload:res})
+    } catch (error) {
+        dispatch({type:"FAIL_GET_PRODUCTS",payload:error.response})
+    }
+}
